d4e49278f4bece45d75ba63b44cdc29baee92837
Gustavo Valiati <gustavovaliati@gmail.com>
2024-09-26 22:18:19 -0300

    Allow full screen

--- main.js
+++ main.js
@@ -3,39 +3,31 @@ const { autoUpdater } = require('electron-updater');
 const log = require('electron-log');
 const path = require('path');
 
-// Criação do servidor WebSocket
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 
-// Função para criar a janela do Electron
 function createWindow() {
-    const win = new BrowserWindow({
+    const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-        },
-        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: true,
             contextIsolation: false
         },
         icon: path.join(__dirname, 'assets/icons/cron-display.png')
     });
 
-    // Carregar a página HTML
-    win.loadFile('index.html');
+    mainWindow.loadFile('index.html');
 
-    // Check for updates
-    autoUpdater.checkForUpdatesAndNotify();
-}
+    mainWindow.webContents.on('before-input-event', (event, input) => {
+        if (input.key === 'F11' && input.type === 'keyDown') {
+            mainWindow.setFullScreen(!mainWindow.isFullScreen());
+            event.preventDefault();
+        }
+    });
 
-// Setup logging for auto-updates
-autoUpdater.logger = log;
-autoUpdater.logger.transports.file.level = 'info';
-
-// Create a custom menu
-const createMenu = () => {
     const menu = Menu.buildFromTemplate([
         {
             label: 'Open',
@@ -53,30 +45,67 @@ const createMenu = () => {
             ]
         },
         {
+            label: 'Window',
+            submenu: [
+                {
+                    label: 'Full Screen',
+                    accelerator: 'F11',
+                    click: () => {
+                        mainWindow.setFullScreen(!mainWindow.isFullScreen());
+                    }
+                }
+            ]
+        },
+        {
             label: 'Help',
             submenu: [
                 {
                     label: 'About',
                     click: () => {
-                        // Add an about section or custom action
-                        console.log('About clicked');
+                        aboutWindow = new BrowserWindow({
+                            width: 600,
+                            height: 300,
+                            title: 'About',
+                            resizable: false,
+                            minimizable: false,
+                            maximizable: false,
+                            alwaysOnTop: true,
+                            webPreferences: {
+                                nodeIntegration: true
+                            }
+                        });
+
+                        aboutWindow.setMenu(null);
+
+                        // Load the about.html file
+                        aboutWindow.loadFile('about.html');
+
+                        aboutWindow.on('closed', () => {
+                            aboutWindow = null;
+                        });
                     }
                 }
             ]
         }
     ]);
     Menu.setApplicationMenu(menu); // Apply the custom menu to the app
-};
 
-// Criar o servidor HTTP para servir o WebSocket
+
+    // Check for updates
+    autoUpdater.checkForUpdatesAndNotify();
+}
+
+// Setup logging for auto-updates
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+
+
 const server = express();
 const httpServer = http.createServer(server);
 const wss = new WebSocket.Server({ server: httpServer });
 
-// Serve a página control.html via Express
 server.use(express.static(path.join(__dirname, 'public')));
 const connectedClients = new Set();
-// WebSocket endpoint
 wss.on('connection', (ws) => {
     connectedClients.add(ws);
     console.log('Novo cliente WebSocket conectado');
@@ -84,7 +113,6 @@ wss.on('connection', (ws) => {
     ws.on('message', (message) => {
         console.log(`Recebido: ${message}`);
 
-        // Enviar a mensagem para todos os outros clientes conectados
         connectedClients.forEach((client) => {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
                 client.send(message);
@@ -92,22 +120,18 @@ wss.on('connection', (ws) => {
         });
     });
 
-    // Remover o cliente da lista ao desconectar
     ws.on('close', () => {
         connectedClients.delete(ws);
         console.log('Cliente WebSocket desconectado');
     });
 });
 
-// Iniciar o servidor WebSocket e HTTP na porta 8085
 httpServer.listen(8085, () => {
-    console.log('Servidor WebSocket e HTTP rodando na porta 8085');
+    console.log('WebSocket and HTTP servers running on port 8085');
 });
 
-// Inicialize a aplicação Electron
 app.whenReady().then(() => {
     createWindow();
-    createMenu();
 });
 
 app.on('window-all-closed', () => {
